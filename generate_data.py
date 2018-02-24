07710ccdacd0dd5355f667869764a0d4e9c5e97d
Danilo Horta <danilo.horta@gmail.com>
2018-02-24 18:54:57 +0000

    save

--- generate_data.py
+++ generate_data.py
@@ -12,15 +12,9 @@ if __name__ == '__main__':
 
     G = random.randn(N, P)
     G /= G.std(0)
-<<<<<<< HEAD
     G -= G.mean(0)
     G /= sqrt(G.shape[1])
     np.save('G', G)
-=======
-    G -= G.mean()
-    G /= np.sqrt(G.shape[1])
-    np.save('null_G', G)
->>>>>>> ee69e3e36f5fa12bd50015ca7f6915c275157eba
 
     h2 = 0.25
 
@@ -35,12 +29,8 @@ if __name__ == '__main__':
     z = 0.0 + u + e
 
     theta = 1 / (1 + np.exp(-z))
-<<<<<<< HEAD
     ntri = random.randint(100, 300, N)
     # ntri = random.randint(10, 300, N)
-=======
-    ntri = random.randint(10, 300, N)
->>>>>>> ee69e3e36f5fa12bd50015ca7f6915c275157eba
     nsuc = random.binomial(ntri, theta)
 
     np.save('null_ntri', ntri)
